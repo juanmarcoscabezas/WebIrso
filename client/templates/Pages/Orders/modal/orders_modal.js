a41675c06ab53403c21ca37aed2b21cb7c69211e
Juan Marcos <juanmarcoscabezas@gmail.com>
2017-11-29 23:38:11 -0500

    mejoras interfaz

--- client/templates/Pages/Orders/modal/orders_modal.js
+++
@@ -1,9 +0,0 @@
-import { Template } from 'meteor/templating';
-import './orders_modal.html';
-
-Template.orders.events({
-  'click #btn-save': function(){
-    Modal.hide('ordersModal');
-    console.log("h");
-  }
-});
